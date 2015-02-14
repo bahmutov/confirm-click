2a0313e1b1fbfa51d1a91d1b7aae4155694a8d2e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-14 16:51:43 -0500

    removing custom alert function, instead using config provider

--- click-app.js
+++ click-app.js
@@ -1,4 +1,9 @@
 angular.module('ClickApp', ['confirm-click'])
+  .config(function (ConfirmClickProvider) {
+    ConfirmClickProvider.set({
+      syncFn: window.confirm.bind(window)
+    });
+  })
   .controller('ClickController', function ($scope) {
     $scope.popAlert = function () {
       /* eslint no-alert:0 */
