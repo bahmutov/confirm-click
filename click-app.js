23cf294b7bdae9e28930b633cbc1382020523966
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-14 17:15:21 -0500

    Updates

--- click-app.js
+++
@@ -1,7 +0,0 @@
-angular.module('ClickApp', ['confirm-click'])
-  .controller('ClickController', function ($scope) {
-    $scope.popAlert = function () {
-      /* eslint no-alert:0 */
-      alert('pop alert!');
-    };
-  });
--- confirm-click.js
+++ confirm-click.js
@@ -1,5 +1,27 @@
 angular.module('confirm-click', [])
-  .directive('confirmClick', ['$window', '$q', '$parse', function ($window, $q, $parse) {
+  .provider('ConfirmClick', function () {
+    var config = {
+      syncFn: window.confirm.bind(window),
+      asyncFn: window.confirm.bind(window)
+    };
+    return {
+      set: function (options) {
+        config.syncFn = options.sync || options.syncFn || config.syncFn;
+        if (typeof config.syncFn !== 'function') {
+          throw new Error('Expected sync confirm function, got ' + JSON.stringify(config.syncFn));
+        }
+
+        config.asyncFn = options.async || options.asyncFn || config.asyncFn;
+        if (typeof config.asyncFn !== 'function') {
+          throw new Error('Expected async confirm function, got ' + JSON.stringify(config.asyncFn));
+        }
+      },
+      $get: function () {
+        return config;
+      }
+    };
+  })
+  .directive('confirmClick', ['$window', '$q', '$parse', 'ConfirmClick', function ($window, $q, $parse, config) {
     var counter = 1;
     return {
       restrict: 'A',
@@ -15,14 +37,13 @@ angular.module('confirm-click', [])
           pre: function (scope, element, attr) {
 
             var question = attr.confirmClick || 'Are you sure?';
-            var ask = $window[attr.confirmFn] || confirm;
 
             if (attr.ngClick && attr.prevClick) {
 
               // strip ()
               var methodName = attr.ngClick.substr(0, attr.ngClick.length - 2);
               scope[methodName] = function (event) {
-                $q.when(ask(question)).then(function (result) {
+                $q.when(config.asyncFn(question)).then(function (result) {
                   if (result) {
                     attr.prevClick(scope, { $event: event });
                   }
@@ -31,7 +52,7 @@ angular.module('confirm-click', [])
             } else {
               // probably regular href links
               element.on('click', function (event) {
-                var answer = ask(question);
+                var answer = config.syncFn(question);
                 if (!answer) {
                   event.preventDefault();
                 }
